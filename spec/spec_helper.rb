7b56b77091e6fb98ff0f5fa8a80ae587a7eeb2d5
ricoch <ricardo@rootstrap.com>
2020-10-01 15:32:12 -0300

    Setup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
-require 'activeadmin-async_exporter'
+ENV['RAILS_ENV'] ||= 'test'
+require 'dummy/config/environment.rb'
+require 'pry'
+require 'active_admin'
+require 'timecop'
+
+require 'simplecov'
+SimpleCov.start
+
+Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
+require 'tmpdir'
+ActiveStorage::Blob.service =
+  ActiveStorage::Service::DiskService.new(root: Dir.mktmpdir('active_storage_tests'))
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+load 'dummy/db/schema.rb'
 
 require 'simplecov'
 SimpleCov.start
@@ -17,3 +36,4 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+

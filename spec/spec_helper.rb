920790295cd1cff8cfff6e6e3d27a349f74a4d8e
ricoch <ricardo@rootstrap.com>
2020-10-16 15:22:31 -0300

    Remove ActiveStorage::Blob.service from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,6 @@ SimpleCov.start
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
 require 'tmpdir'
-ActiveStorage::Blob.service =
-  ActiveStorage::Service::DiskService.new(root: Dir.mktmpdir('active_storage_tests'))
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
